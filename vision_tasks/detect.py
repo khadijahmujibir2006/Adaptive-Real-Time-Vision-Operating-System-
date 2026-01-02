3975976028a99a56ca9b11376644d89257807cd6
khadijahmujibir2006 <khadijahmujibir@gmail.com>
2026-01-02 20:44:30 +0530

    Update detect.py

--- vision_tasks/detect.py
+++ vision_tasks/detect.py
@@ -4,7 +4,7 @@ import cv2
 def detect(frame):
     start = time.time()
 
-    # Simple placeholder detection (face detector)
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
